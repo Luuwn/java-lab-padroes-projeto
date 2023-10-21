69c63312169114eb035d656d784e2ead2e9d3eaf
Luan Costa <112595656+luuwn@users.noreply.github.com>
2023-10-21 23:38:03 +0000

    update: design pattern Singleton

--- padraoProjetoDIO/src/AppTest.java
+++ padraoProjetoDIO/src/AppTest.java
@@ -1,7 +1,29 @@
+import Padroes.Singleton.SingletonLazy;
+import Padroes.Singleton.SingletonLazyHolder;
+import Padroes.Singleton.SingletonEager;
+
 public class AppTest {
     public static void main(String[] args) throws Exception {
-        SingletonLazy lazy = SingletonLazy.getInstancia();
+
+        //Testes relacionados ao design pattern singleton
         
+        //Lazy
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        //Eager
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        //LazyHolder
+        SingletonLazyHolder lazyH = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyH);
+        lazyH = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyH);
 
     }
 }
